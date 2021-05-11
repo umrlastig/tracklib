b495ef14812be565b8e720e605ebbdd0eaad7e77
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-05-11 22:10:07 +0200

    pb import

--- tracklib/algo/Cinematics.py
+++ tracklib/algo/Cinematics.py
@@ -2,21 +2,8 @@
 # Class to manage cinematic computations on GPS tracks
 # -----------------------------------------------------------------------------
 
-import sys
-import progressbar
-import numpy as np
-
-
-from tracklib.core.Obs import Obs
-from tracklib.core.TrackCollection import TrackCollection
-from tracklib.core.Kernel import GaussianKernel
-
-import tracklib.core.Utils as utils
 import tracklib.algo.Analytics as Analytics
-import tracklib.algo.Geometrics as Geometrics
-import tracklib.algo.Interpolation as Interpolation
 import tracklib.core.Operator as Operator
-import tracklib.core.Kernel as Kernel
 
 
     
