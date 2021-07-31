55a9e0eac81907fe703947ac5c851c5abec201dc
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-07-31 20:23:42 +0200

    add class to write network in file

---
+++ tracklib/io/NetworkWriter.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+
+class NetworkWriter:
+
+    @staticmethod
+    def writeToCsv(network, path="", separator=",", h=1):
+        
+        if h == 1:
+            output  = "link_id" + separator
+            output += "source" + separator
+            output += "target" + separator
+            output += "direction" + separator
+            output += "wkt\n"
+        else:
+            output  = ""
+            
+        for idedge in network.EDGES:
+            edge = network.EDGES[idedge]
+            output += edge.id + separator
+            output += edge.source.id + separator
+            output += edge.target.id + separator
+            output += str(edge.orientation) + separator
+            output += "\"" + edge.geom.toWKT() +"\""
+            output += "\n"
+        
+        if path:
+            f = open(path, "w")
+            f.write(output)
+            f.close()
+            
+        return output
