b31cdd2ac3cf3718c54c85262c5fb1a744e2796f
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-08-10 22:00:20 +0200

    pb chemin pour fichier données

--- test/io/test_filereader.py
+++ test/io/test_filereader.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import os.path
+from unittest import TestCase, TestSuite, TextTestRunner
 
 from tracklib.io.FileReader import FileReader
 from tracklib.core.TrackCollection import TrackCollection
 
-class TestFileReader(unittest.TestCase):
+class TestFileReader(TestCase):
+    
+    def setUp (self):
+        self.resource_path = os.path.join(os.path.split(__file__)[0], "../..")
 
     def test_read_wkt_polygon(self):
         # =============================================================
-        csvpath = '../../data/wkt/bati.wkt'
+        csvpath = os.path.join(self.resource_path, 'data/wkt/bati.wkt')
         TRACES = FileReader.readFromWKTFile(csvpath, 0)
         self.assertIsInstance(TRACES, TrackCollection)
         self.assertEqual(2312, TRACES.size())
 
     def test_read_wkt_linestring(self):
         # =============================================================
-        csvpath = '../../data/wkt/iti.wkt'
+        csvpath = os.path.join(self.resource_path, 'data/wkt/iti.wkt')
         TRACES = FileReader.readFromWKTFile(csvpath, 0, -1, -1, "#", 1, "ENUCoords", None, True)
         # id_user=-1, id_track=-1, separator=";", h=0, srid="ENUCoords", bboxFilter=None
         self.assertIsInstance(TRACES, TrackCollection)
@@ -26,9 +30,9 @@ class TestFileReader(unittest.TestCase):
 
 if __name__ == '__main__':
     #unittest.main()
-    suite = unittest.TestSuite()
+    suite = TestSuite()
     suite.addTest(TestFileReader("test_read_wkt_polygon"))
     suite.addTest(TestFileReader("test_read_wkt_linestring"))
-    runner = unittest.TextTestRunner()
+    runner = TextTestRunner()
     runner.run(suite)
 
