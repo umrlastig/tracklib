11132911b37d0793b0fbf8ea274bed2b8a7c69ea
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-08-13 17:36:09 +0200

    change source code of summarizing

--- test/core/test_grille.py
+++ test/core/test_grille.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from unittest import TestCase, TestSuite, TextTestRunner
+import matplotlib.pyplot as plt
 
-from tracklib.core import (
-  Track, Obs, Coords, GPSTime, Grid)
+from tracklib.core import (Track, Obs, Coords, GPSTime, Grid)
 from tracklib.algo import (Analytics)
 from tracklib.algo import (Summarising) 
 from tracklib.core.TrackCollection import TrackCollection
@@ -16,70 +16,99 @@ class TestGrille(TestCase):
         GPSTime.GPSTime.setReadFormat("4Y-2M-2D 2h:2m:2s")
         self.TRACES = []
         
+        # ---------------------------------------------------------------------
         trace1 = Track.Track([], 1)
         c1 = Coords.ENUCoords(10, 10, 0)
         p1 = Obs.Obs(c1, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:00"))
         trace1.addObs(p1)
+        
         c2 = Coords.ENUCoords(10, 110, 0)
         p2 = Obs.Obs(c2, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:12"))
         trace1.addObs(p2)
-        c3 = Coords.ENUCoords(310, 110, 0)
+        
+        c3 = Coords.ENUCoords(270, 110, 0)
         p3 = Obs.Obs(c3, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:40"))
         trace1.addObs(p3)
+        
         c4 = Coords.ENUCoords(360, 210, 0)
         p4 = Obs.Obs(c4, GPSTime.GPSTime.readTimestamp("2018-01-01 10:01:50"))
         trace1.addObs(p4)
+        
         self.TRACES.append(trace1)
         
+        # ---------------------------------------------------------------------
         trace2 = Track.Track([], 2)
-        trace2.addObs(p1)
-        trace2.addObs(p2)
+        c7 = Coords.ENUCoords(25, 10, 0)
+        p7 = Obs.Obs(c7, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:15"))
+        trace2.addObs(p7)
+        
+        c6 = Coords.ENUCoords(280, 90, 0)
+        p6 = Obs.Obs(c6, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:45"))
+        trace2.addObs(p6)
+        
+        c5 = Coords.ENUCoords(330, 20, 0)
+        p5 = Obs.Obs(c5, GPSTime.GPSTime.readTimestamp("2018-01-01 10:01:55"))
+        trace2.addObs(p5)
+        
         self.TRACES.append(trace2)
         
-        Xmin = 0
-        Xmax = 400
-        Ymin = 0
-        Ymax = 250
-            
-        PixelSize = 50
+        self.collection = TrackCollection(self.TRACES)
         
         #  Construction de la grille
-        self.grille = Grid.Grid(Xmin, Ymin, Xmax, Ymax, PixelSize)
+        #self.grille = Grid.Grid(Xmin, Ymin, Xmax, Ymax, PixelSize)
+        marge = 0.05
+        self.grille = Grid.Grid(self.collection, (60,60), marge)
+        self.grille.plot()
+        plt.show()
+        
+        self.assertEqual(self.grille.xmin, 10.0 - marge*(360-10))
+        self.assertEqual(self.grille.xmax, 360.0 + marge*(360-10))
+        self.assertEqual(self.grille.ymin, 10.0 - marge*(210-10))
+        self.assertEqual(self.grille.ymax, 210.0 + marge*(210-10))
+        
+        self.assertEqual(self.grille.ncol, int (((360-10) + marge*(360-10)) / 60))
+        self.assertEqual(self.grille.nrow, int (((210-10) + marge*(210-10)) / 60))
+    
+        self.assertEqual(self.grille.XPixelSize, ((360-10) + 2*marge*(360-10)) / self.grille.ncol)
+        self.assertEqual(self.grille.YPixelSize, ((210-10) + 2*marge*(210-10)) / self.grille.nrow)
     
     
     def test_summarize_af(self):
         
-        collection = TrackCollection(self.TRACES)
-        collection.addAnalyticalFeature(Analytics.speed)
-
-        # 
+        self.collection.addAnalyticalFeature(Analytics.speed)
+        
         af_algos = ['speed'] #, utils.stop_point]
         cell_operators = [Summarising.co_avg] #, utils.sum]
-        self.grille.addAnalyticalFunctionForSummarize(collection, af_algos, cell_operators)
         
-        #
-        self.grille.plot(Analytics.speed, Summarising.co_avg)
-        #print ('')
+        raster = Summarising.summarize(self.grille, af_algos, cell_operators)
+        raster.plot(Analytics.speed, Summarising.co_avg)
         
-        sumPlot = self.grille.buildArray(Analytics.speed, Summarising.co_avg)
+        rasterBand = raster.getRasterBand(Analytics.speed, Summarising.co_avg)
         
-        self.assertEqual(sumPlot[0][0][0], 0)
-        self.assertEqual(sumPlot[1][0][0], 0)
-        self.assertEqual(sumPlot[2][0][0], 8)
-        self.assertEqual(sumPlot[3][0][0], 0)
-        self.assertEqual(sumPlot[4][0][0], 8)
+        speedTrace1 = self.collection.getTrack(0).getAnalyticalFeature('speed')
+        speedTrace2 = self.collection.getTrack(1).getAnalyticalFeature('speed')
         
-        self.assertEqual(sumPlot[0][6][0], 0)
-        self.assertEqual(sumPlot[1][6][0], 0)
-        self.assertEqual(sumPlot[2][6][0], 3)
-        self.assertEqual(sumPlot[3][6][0], 0)
-        self.assertEqual(sumPlot[4][6][0], 0)
+        self.assertEqual(rasterBand[0][0], 0)
+        self.assertEqual(rasterBand[1][0], speedTrace1[1])
+        self.assertEqual(rasterBand[2][0], (speedTrace1[0] + speedTrace2[0]) / 2)
         
-        self.assertEqual(sumPlot[0][7][0], 1)
-        self.assertEqual(sumPlot[1][7][0], 0)
-        self.assertEqual(sumPlot[2][7][0], 0)
-        self.assertEqual(sumPlot[3][7][0], 0)
-        self.assertEqual(sumPlot[4][7][0], 0)
+        self.assertEqual(rasterBand[0][1], 0)
+        self.assertEqual(rasterBand[1][1], 0)
+        self.assertEqual(rasterBand[2][1], 0)
+        self.assertEqual(rasterBand[0][2], 0)
+        self.assertEqual(rasterBand[1][2], 0)
+        self.assertEqual(rasterBand[2][2], 0)
+        self.assertEqual(rasterBand[0][3], 0)
+        self.assertEqual(rasterBand[1][3], 0)
+        self.assertEqual(rasterBand[2][3], 0)
+        
+        self.assertEqual(rasterBand[0][4], 0)
+        self.assertEqual(rasterBand[1][4], (speedTrace1[2] + speedTrace2[1]) / 2)
+        self.assertEqual(rasterBand[2][4], 0)
+        
+        self.assertEqual(rasterBand[0][5], speedTrace1[3])
+        self.assertEqual(rasterBand[1][5], 0)
+        self.assertEqual(rasterBand[2][5], speedTrace2[2])
         
     
     def test_summarize_aa(self):
@@ -181,10 +210,10 @@ class TestGrille(TestCase):
 
 if __name__ == '__main__':
     suite = TestSuite()
-    suite.addTest(TestGrille("test_summarize_aa"))
+    #suite.addTest(TestGrille("test_summarize_aa"))
     suite.addTest(TestGrille("test_summarize_af"))
-    suite.addTest(TestGrille("test_sum_trace"))
-    suite.addTest(TestGrille("test_mixte_aa_af_uid"))
+    #suite.addTest(TestGrille("test_sum_trace"))
+    #suite.addTest(TestGrille("test_mixte_aa_af_uid"))
     runner = TextTestRunner()
     runner.run(suite)
     
--- tracklib/core/Grid.py
+++ tracklib/core/Grid.py
@@ -6,341 +6,111 @@
 #     Opérations avec ....
 #
 # ----------------------------------------------------------------
-from datetime import datetime
-import math
 import matplotlib.pyplot as plt
-import numpy as np
-from skimage import io
-#from PIL import Image
 
-import tracklib.core.Utils as utils
-
-
-class Cell:
-    '''
-    '''
-    
-    def __init__(self, line_pos, col_pos, af_names, cles):
-        
-        self.line_pos = line_pos
-        self.col_pos = col_pos
-        
-        # List of values of each observations for algo_names (speed, stop)
-        self.vals = {} 
-        for name in af_names:
-            self.vals[name] = []
-            
-        # Dictionary for an af value is the aggregate
-        self.af_agg = {} #
-        for cle in cles:
-            self.af_agg[cle] = Grid.NO_DATA_VALUE
-        
-        
-        
-    def compute(self, cle, aggregate):
-        
-        tabnames = cle.split('#')
-        tarray = self.vals[tabnames[0]]
-        sumval = aggregate(tarray)
-        self.af_agg[cle] = sumval
-                
-        
-        
-
+from tracklib.core.Bbox import Bbox
 
 
 class Grid:
     """ 
-        Une grille est définie par son coin inférieur droit, 
-        sa taille et la taille des cellules.
-        Elle contient des SummarizeTable (tableau des valeurs et )
-     """
+        Pour IndexSpatial et Summarizng
+        
+        grid[icol][jrow] = list or dict
+    """
     
-    NO_DATA_VALUE = 0  #-99999.00
-    
-    
-    def __init__(self, Xmin, Ymin, Xmax, Ymax, XPixelSize, YPixelSize = None):
+    def __init__(self, collection, resolution=None, margin=0.05, verbose=True):
         '''
         Grid constructor. 
-        The origin is the lower-left corner of the grid.
         
-        :param XOrigin: the first coordinate of the origin
-        :param YOrigin: the second coordinate of the origin
-        :param XSize: raster width
-        :param YSize: raster height
-        :param XPixelSize: 
+        Margin : relative float. Default value is +5%
         
-        :type XOrigin: double
-        :type YOrigin: double
         '''
-        
-        self.XSize = Xmax - Xmin
-        self.YSize = Ymax - Ymin
-        
-        self.XOrigin = Xmin 
-        self.YOrigin = Ymin + self.YSize
-        
-        self.XPixelSize = XPixelSize
-        if YPixelSize == None:
-            self.YPixelSize = XPixelSize
+        # Bbox only or collection
+        if isinstance(collection, Bbox):
+            bb = collection
         else:
-            self.YPixelSize = YPixelSize
-        
-        # 
-        self.ncol = math.ceil(self.XSize / self.XPixelSize)
-        self.nrow = math.ceil(self.YSize / self.YPixelSize)
-        
-        self.color1 = (0,0,0)
-        self.color2 = (255,255,255)
-        
-        
-    def setColor(self, c1, c2):
-        self.color1 = c1
-        self.color2 = c2
-        
-        
-    def addAnalyticalFunctionForSummarize(self, collection, af_algos, aggregates):
-
-        if not isinstance(af_algos, list):
-            af_algos = [af_algos]
-        if not isinstance(aggregates, list):
-            aggregates = [aggregates]
-
-        if len(af_algos) == 0:
-            print("Error: af_algos is empty")
-            return 0
-        
-        if len(af_algos) != len(aggregates):
-            print("Error: af_names and aggregates must have the same number elements")
-            return 0
-        
-        # ---------------------------------------------------------------------
-        # Tableau des noms
-        self.af_names = [] # Utile pour stocker une seule fois l'af
-        self.__summarizeFields = {}  # cle nom_algo#nom_agg
-        
-        for idx, af_algo in enumerate(af_algos):
+            bb = collection.bbox()
             
-            aggregate = aggregates[idx]
-            
-            if isinstance(af_algo, str):
-                name = af_algo
-            else:
-                name = af_algo.__name__
-                
-            cle = name + '#' + aggregate.__name__
-            
-            if name not in self.af_names:
-                self.af_names.append(name)
-            if cle not in self.__summarizeFields.keys():
-                self.__summarizeFields[cle] = aggregate
-        
-        # ---------------------------------------------------------------------        
-        #  On construit des cellules vides
-        self.tabcel = []
-        for i in range(self.nrow):
-            self.tabcel.append([])
-            for j in range(self.ncol):
-                self.tabcel[i].append(Cell(i, j, self.af_names, self.__summarizeFields.keys()))
-        
-        # ---------------------------------------------------------------------
-        #  On ajoute les valeurs des af dans les cellules
-        for af_algo in af_algos:
-            self.__addAFValueInCell(collection, af_algo)
+        bb = bb.copy()
+        bb.addMargin(margin)
+        (self.xmin, self.xmax, self.ymin, self.ymax) = bb.asTuple()
 
-        # On calcule les agregats            
-        for cle in self.__summarizeFields.keys():
-            self.__summarize(cle, self.__summarizeFields[cle])
-              
-
-                    
-            
-    def __addAFValueInCell(self, collection, af_algo):
-        ''' 
-        On dispatch les valeurs de l'AF dans les cellules.
-        Avant on vérifie si l'AF existe, sinon on la calcule.
-        '''
+        ax, ay = bb.getDimensions()
         
-        if not isinstance(af_algo, str):
-            af_name = af_algo.__name__
+        if resolution is None:
+            am = max(ax, ay)
+            r = am/100
+            resolution = (int(ax/r), int(ay/r))
         else:
-            af_name = af_algo
+            r = resolution
+            resolution = (int(ax/r[0]), int(ay/r[1]))
+
+        self.collection = collection
         
-        for trace in collection.getTracks():
-            
-            if not isinstance(af_algo, str):
-                # On calcule l'AF si ce n'est pas fait
-                trace.addAnalyticalFeature(af_algo)
-            
-            # On eparpille dans les cellules
-            for i in range(trace.size()):
-                obs = trace.getObs(i)
+        # Nombre de dalles par cote
+        self.ncol = resolution[0]
+        self.nrow = resolution[1]
+        
+        # Tableau de collections de features appartenant a chaque dalle. 
+        # Un feature peut appartenir a plusieurs dalles.
+        self.grid = []
+        for i in range(self.ncol):
+            self.grid.append([])
+            for j in range(self.nrow):
+                self.grid[i].append([])
                 
-                # On cherche la cellule de la grille ou est le point
-                X = float(obs.position.getX()) - self.XOrigin
-                column = math.floor(X / self.XPixelSize)
+        self.XPixelSize = ax / self.ncol
+        self.YPixelSize = ay / self.nrow
+        
+        
+        
+    # ------------------------------------------------------------
+    # Normalized coordinates of coord: (x,) -> (i,j) with:
+    #   i = (x-xmin)/(xmax-xmin)*nb_cols
+    #   j = (y-ymin)/(ymax-ymin)*nb_rows
+    # Returns None if out of grid
+    # ------------------------------------------------------------
+    def getCell(self, coord):
+        
+        if (coord.getX() < self.xmin) or (coord.getX() > self.xmax):
+            overflow = '{:5.5f}'.format(max(self.xmin-coord.getX(), coord.getX()-self.xmax))
+            print('Warning: x overflow '+str(coord)+"  OVERFLOW = "+str(overflow))
+            return None
+        if (coord.getY() < self.ymin) or (coord.getY() > self.ymax):
+            overflow = '{:5.5f}'.format(max(self.ymin-coord.getY(), coord.getY()-self.ymax))
+            print('Warning: y overflow '+str(coord)+"  OVERFLOW = "+str(overflow))
+            return None
+
+        idx = (float(coord.getX()) - self.xmin) / self.XPixelSize
+        idy = (float(coord.getY()) - self.ymin) / self.YPixelSize
                 
-                #Y = float(obs.position.getY()) - self.YOrigin
-                Y = self.YOrigin - float(obs.position.getY())
-                line = math.floor(Y / self.YPixelSize)
-
-                if (0 <= column and column < self.ncol and 0 <= line and line < self.nrow):
-
-                    if not isinstance(af_algo, str):
-                        val = trace.getObsAnalyticalFeature(af_name, i)
-                    elif af_algo != "uid":
-                        val = trace.getObsAnalyticalFeature(af_algo, i)
-                    else:
-                        val = trace.uid
-                    
-                    self.tabcel[line][column].vals[af_name].append(val)
-
-##                else:
-##                    print ("Warning: position outer of zone. " \
-##                       + "Row: [0, " + str(line) + ', ' + str(self.nrow) + '], ' \
-##                       + "Column: [0, " + str(column) + ', ' + str(self.ncol) + ']' + str(obs.position))
-##    
-                    
-                    
-    def __summarize(self, cle, aggregate):
-       for i in range(self.nrow):
-           for j in range(self.ncol):
-                cell = self.tabcel[i][j]
-                cell.compute(cle, aggregate)
-                
-                
-    
-    def buildArray(self, af_algo, aggregate, valmax = None, startpixel = 0):
-        
-        if not isinstance(af_algo, str):
-            name = af_algo.__name__ + '#' + aggregate.__name__
-        else:
-            name = 'uid' + '#' + aggregate.__name__
-        
-        sumPlot = np.zeros((self.nrow, self.ncol, len(self.__summarizeFields)), dtype='uint8')    
-        for i in range(self.nrow):
-            for j in range(self.ncol):
-                val = self.tabcel[i][j].af_agg[name]
-                if utils.isnan(val):
-                    val = 0
-                elif valmax != None and val > valmax:
-                    val = startpixel
-                else:
-                    if valmax != None:
-                        val = int(startpixel + (255 - startpixel) * (valmax - val) / valmax)
-
-                sumPlot[i][j][0] = val
-    
-        return sumPlot
-    
-    
-    def plot (self, af_algo, aggregate, valmax = None, startpixel = 0):
-        
-        if af_algo != 'uid':
-            name = af_algo.__name__ + '#' + aggregate.__name__
-        else:
-            name = 'uid' + '#' + aggregate.__name__
-        
-        sumPlot = self.buildArray(af_algo, aggregate, valmax, startpixel)
-        
-        cmap = utils.getOffsetColorMap(self.color1, self.color2, startpixel / 255)
-        plt.imshow(sumPlot[:,:,0], cmap=cmap)
-        plt.title(name)
-        plt.colorbar()
-        plt.show()
-        
-        
-        
-    
-    def saveGrid(self, filename, af_algo, aggregate, valmax = None, startpixel = 0):
-        
-        sumPlot = self.buildArray(af_algo, aggregate, valmax, startpixel)
-        io.imsave(filename, sumPlot)
-        
-        
-#    def boxplot(self, af_algo, aggregate):
-#        name = af_algo.__name__ + '#' + aggregate.__name__
-#        k = self.__summarizeFieldDico[name]
-#        
-#        sumPlot = np.zeros((self.nrow, self.ncol, len(self.__summarizeFieldDico)), dtype='float')    
-#        for i in range(self.nrow):
-#            for j in range(self.ncol):
-#                val = self.sum[i][j][k]
-#                if utils.isnan(val):
-#                    val = 0
-#                sumPlot[i][j][k] = val
-#        
-#        sumPlot = self.__getSumArray(af_algo, aggregate)
-#        
-#        plt.boxplot(sumPlot[:,:,k], vert=False)
-        
-
-    def exportToAsc(self, path, af, aggregate):
-        
-        name = af.__name__ + '#' + aggregate.__name__
-        filepath = path + name + "_" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".asc"
-
-#        k = self.__summarizeFieldDico[name]
-#        
-#        ascContent = 'ncols\t\t' + str(self.ncol) + '\n'
-#        ascContent = ascContent + 'nrows\t\t' + str(self.nrow) + '\n'
-#        ascContent = ascContent + 'xllcorner\t' + str(self.XOrigin) + '\n'
-#        ascContent = ascContent + 'yllcorner\t' + str(self.YOrigin) + '\n'
-#        ascContent = ascContent + 'cellsize\t' + str(self.XPixelSize) + '\n'
-#        ascContent = ascContent + 'NODATA_value\t' + str(Grid.NO_DATA_VALUE) + '\n'
-#        
-#        for i in range(self.nrow):
-#            for j in range(self.ncol):
-#                if j > 0:
-#                    ascContent = ascContent + '\t'
-#                val = self.sum[i][j][k]
-#                if utils.isnan(val):
-#                    val = Grid.NO_DATA_VALUE
-#                ascContent = ascContent + str(val)
-#            ascContent = ascContent + '\n'
-#    
-#        try:
-#            f = open(filepath, "a")
-#            f.write(ascContent)
-#            f.close() 
-#        except:
-#            raise Warning("impossible d'écrire le fichier asc")
-    
-    
-
+        return (idx, idy)
 
         
+    def plot(self, base=True, af_algo = None, aggregate = None):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, xlim=(self.xmin, self.xmax), ylim=(self.ymin, self.ymax))
         
-#    def plotImage3AF(self, afs_algo, aggs):
-#        w, h = self.ncol, self.nrow
-#        t = (h, w, 3)
-#        A = np.zeros(t, dtype=np.uint8)
-#        
-#        maxs = []
-#        #maxs.append(utils.co_max()
-#        #        self.track.operate(Operator.Operator.MAX, afs_algo[0]))
-#        
-#        for i in range(h):
-#            for j in range(w):
-#                rgb = [100, 155, 3]
-#                for k in range(len(afs_algo)):
-#                    af_algo = afs_algo[k]
-#                    aggregate = aggs[k]
-#                    
-#                    name = af_algo.__name__ + '#' + aggregate.__name__
-#                    ind = self.__summarizeFieldDico[name]
-#                    
-#                    val = self.sum[i][j][ind]
-#                    if utils.isnan(val):
-#                        val = 0
-#                 
-#                A[i,j] = rgb
-#        
-#        #i = Image.fromarray(A, "RGB")
-#        # im = Image.new(mode = "RGB", size = (200, 200) )
-#        im = Image.fromarray(A)
-#        im.show()
+        for i in range(1,self.ncol):
+            xi = i*self.XPixelSize + self.xmin
+            ax.plot([xi,xi], [self.ymin, self.ymax], '-',color='lightgray')
+        for j in range(1,self.nrow):
+            yj = j*self.YPixelSize + self.ymin
+            ax.plot([self.xmin, self.xmax], [yj,yj], '-',color='lightgray')
         
-        
-        
-        
+        if base:
+            self.collection.plot(append=ax)
+
+        for i in range(self.ncol):
+            xi1 = i*self.XPixelSize + self.xmin; xi2 = xi1 + self.XPixelSize
+            for j in range(self.nrow):
+                yj1 = j*self.YPixelSize + self.ymin; yj2 = yj1 + self.YPixelSize
+                if len(self.grid[i][j]) > 0:
+                    print (self.grid[i][j])
+                    polygon = plt.Polygon(
+                         [[xi1, yj1], [xi2, yj1], [xi2, yj2], [xi1, yj2], [xi1, yj1]])
+                    ax.add_patch(polygon)
+                    polygon.set_facecolor('lightcyan')
+
+
