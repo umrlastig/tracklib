30b7ef13d75f9ff10747a569a24eb3429520392b
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-10-08 17:01:23 +0200

    factorise creation netwoork dans IGNReader et NetworkReader

--- tracklib/io/NetworkFormat.py
+++ tracklib/io/NetworkFormat.py
@@ -10,7 +10,15 @@ class NetworkFormat:
     # -------------------------------------------------------------
     # Load file format from network_file_format
     # -------------------------------------------------------------
-    def __init__(self, name):
+    def __init__(self, name = None):
+        
+        if name != None and name != '':
+            self.createFromFile(name)
+        else:
+            self.name = 'UNDEFINED'
+            
+            
+    def createFromFile(self, name):
         
         FIELDS = []
         with open(NetworkFormat.NETWORK_FILE_FORMAT) as ffmt:
@@ -47,4 +55,33 @@ class NetworkFormat:
         self.separator = FIELDS[7].strip()
         self.separator = self.separator.replace("b", " ")
         self.separator = self.separator.replace("c", ",")
-        self.separator = self.separator.replace("s", ";")	
+        self.separator = self.separator.replace("s", ";")
+    
+    
+    '''
+    '''
+    def createFromDict(self, param):
+        
+        if param['name'] != None and param['name'] != '':
+            self.name = param['name']
+        
+        if param['pos_edge_id'] != None and param['pos_edge_id'] != '':
+            self.pos_edge_id = param['pos_edge_id']
+        
+        if param['pos_source'] != None and param['pos_source'] != '':
+            self.pos_source = param['pos_source']
+            
+        if param['pos_target'] != None and param['pos_target'] != '':
+            self.pos_target = param['pos_target']
+            
+        if param['pos_wkt'] != None and param['pos_wkt'] != '':
+            self.pos_wkt = param['pos_wkt']
+            
+        if param['pos_poids'] != None and param['pos_poids'] != '':
+            self.pos_poids = param['pos_poids']
+            
+        if param['pos_sens'] != None and param['pos_sens'] != '':
+            self.pos_sens = param['pos_sens']
+            
+        if param['srid'] != None and param['srid'] != '':
+            self.srid = param['srid']
--- tracklib/io/NetworkReader.py
+++ tracklib/io/NetworkReader.py
@@ -13,6 +13,8 @@ import tracklib.algo.Cinematics as Cinematics
 
 
 class NetworkReader:
+    
+    counter = 0
 
     @staticmethod
     def readFromFile(path, formatfile = 'DEFAULT', verbose=True):
@@ -21,7 +23,7 @@ class NetworkReader:
         fmt = NetworkFormat(formatfile)
         
         num_lines = sum(1 for line in open(path))
-		
+
         if verbose:
             print("Loading network...")
 
@@ -36,159 +38,65 @@ class NetworkReader:
                 if cpt >= fmt.h:
                     break
     
-            counter = 0    
             if verbose:
                 spamreader = progressbar.progressbar(spamreader, max_value=num_lines)
                 
             for row in spamreader:
-                
-                edge_id = str(row[fmt.pos_edge_id])
-                if fmt.pos_edge_id < 0:
-                    edge_id = counter
-                counter = counter + 1
-
-                geom = str(row[fmt.pos_wkt])
-                TAB_OBS = wktLineStringToObs(geom, fmt.srid.upper())
-
-                
-                # Au moins 2 points
-                if len(TAB_OBS) < 2:
-                    continue
-                
-                track = Track(TAB_OBS)
-                Cinematics.computeAbsCurv(track)
-
-                edge = Edge(edge_id, track)
-              
-                # Orientation
-                orientation = int(row[fmt.pos_sens])
-                if (orientation not in [Edge.DOUBLE_SENS, Edge.SENS_DIRECT, Edge.SENS_INVERSE]):
-                    orientation = Edge.DOUBLE_SENS
-                edge.orientation = orientation
-                  
-                # Poids
-                if fmt.pos_poids == -1:
-                    poids = track.length()
-                else:
-                    poids = float(row[fmt.pos_poids])
-                edge.weight = poids
-                
-                 # source node 
-                source = str(row[fmt.pos_source])
-                noeudIni = Node(source, track.getFirstObs().position)
-                
-                # target node 
-                target = str(row[fmt.pos_target])
-                noeudFin = Node(target, track.getLastObs().position)
-                
-               # Add edge
+                (edge, noeudIni, noeudFin) = readLineAndAddToNetwork(row, fmt)
                 network.addEdge(edge, noeudIni, noeudFin)
 
-        csvfile.close()      
+        csvfile.close()
                 
         # Return network loaded
         return network
 
 
-
+def readLineAndAddToNetwork(row, fmt):
     
-    # @staticmethod
-    # def readFromFile2(path, formatfile = 'DEFAULT', verbose=True):
-    #     '''
-    #     TODO : posSol
-    #     '''
-        
-    #     fmt = NetworkFormat(formatfile)  # Read from input parameters
-        
-    #     network = Network()
-        
-    #     num_lines = sum(1 for line in open(path))
+    edge_id = str(row[fmt.pos_edge_id])
+    if fmt.pos_edge_id < 0:
+        edge_id = NetworkReader.counter
+    NetworkReader.counter = NetworkReader.counter + 1
 
-        
-    #     with open(path, encoding='utf-8') as csvfile:
-            
-    #         spamreader = csv.reader(csvfile, delimiter=fmt.separator, doublequote= True, )
-            
-    #         # Header
-    #         cpt = 0
-    #         for row in spamreader:
-    #             cpt += 1
-    #             if cpt >= fmt.h:
-    #                 break
-    
-    #         counter = 0    
-    #         if verbose:
-    #             spamreader = progressbar.progressbar(spamreader, max_value=num_lines)
-                
-    #         for row in spamreader:
-                
-    #             edge_id = str(row[fmt.pos_edge_id])
-    #             if fmt.pos_edge_id < 0:
-    #                 edge_id = counter
-    #             counter = counter + 1
+    geom = str(row[fmt.pos_wkt])
+    TAB_OBS = wktLineStringToObs(geom, fmt.srid.upper())
 
-    #             geom = str(row[fmt.pos_wkt])
-    #             TAB_OBS = wktLineStringToObs(geom, fmt.srid.upper())
+        # Au moins 2 points
+    if len(TAB_OBS) < 2:
+        return
+                
+    track = Track(TAB_OBS)
+    Cinematics.computeAbsCurv(track)
 
-                
-    #             # Au moins 2 points
-    #             if len(TAB_OBS) < 2:
-    #                 continue
-                
-    #             track = Track(TAB_OBS)
-                
-    #             # Transformation GEO coordinates to ENU
-    #             #if (fmt.srid.upper() in ["GEOCOORDS", "GEO", "ECEFCOORDS", "ECEF"]):
-    #             #    track.toENUCoords()
-                
-    #             edge = Edge(edge_id, track)
-              
-    #             # Orientation
-    #             orientation = int(row[fmt.pos_sens])
-    #             if (orientation not in [Edge.DOUBLE_SENS, Edge.SENS_DIRECT, Edge.SENS_INVERSE]):
-    #                 orientation = Edge.DOUBLE_SENS
-    #             edge.setOrientation(orientation)
-                  
-    #             # Poids
-    #             if fmt.pos_poids == -1:
-    #                 poids = track.length()
-    #             else:
-    #                 poids = float(row[fmt.pos_poids])
-    #             edge.setPoids(poids)
-                
-    #              # source node 
-    #             source = str(row[fmt.pos_source])
-    #             noeudIni = Node(source, track.getFirstObs().position)
-    #             if noeudIni.id not in network.NODES:
-    #                 edge.setNoeudIni(noeudIni)
-    #                 network.addNode(noeudIni)
-    #             else:
-    #                 n = network.getNode(source)
-    #                 edge.setNoeudIni(n)
-                    
-                    
-    #             # target node 
-    #             target = str(row[fmt.pos_target])
-    #             noeudFin = Node(target, track.getLastObs().position)
-    #             if noeudFin.id not in network.NODES:
-    #                 edge.setNoeudFin(noeudFin)
-    #                 network.addNode(noeudFin)
-    #             else:
-    #                 n = network.getNode(target)
-    #                 edge.setNoeudFin(n)
-                    
-                
-    #            # Add edge
-    #             network.addEdge(edge)
+    edge = Edge(edge_id, track)
 
-    #     csvfile.close()      
-                
-    #     # Return network loaded
-    #     return network
-        
+    # Orientation
+    orientation = int(row[fmt.pos_sens])
+    if (orientation not in [Edge.DOUBLE_SENS, Edge.SENS_DIRECT, Edge.SENS_INVERSE]):
+        orientation = Edge.DOUBLE_SENS
+    edge.orientation = orientation
 
-   
-    
+    # Poids
+    if fmt.pos_poids == -1:
+        poids = track.length()
+    else:
+        poids = float(row[fmt.pos_poids])
+    edge.weight = poids
+
+    # Source node
+    source = str(row[fmt.pos_source])
+    noeudIni = Node(source, track.getFirstObs().position)
+
+    # Target node 
+    target = str(row[fmt.pos_target])
+    noeudFin = Node(target, track.getLastObs().position)
+
+    # Return
+    return (edge, noeudIni, noeudFin)
+    # network.addEdge(edge, noeudIni, noeudFin)
+
+
+
 def wktLineStringToObs(wkt, srid):
     '''
     Une polyligne de n points est modélisée par une Track (timestamp = 1970/01/01 00 :00 :00)
