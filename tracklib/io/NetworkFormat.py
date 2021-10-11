d4f46b305a9a195f9d0d3571c9e0553ba54cee20
Mattia Bunel <mattia.bunel@ign.fr>
2021-10-11 15:36:49 +0200

    Formatage sphix

--- tracklib/io/NetworkFormat.py
+++ tracklib/io/NetworkFormat.py
@@ -2,6 +2,7 @@
 
 import os.path
 
+
 class NetworkFormat:
 
     resource_path = os.path.join(os.path.split(__file__)[0], "../..")
@@ -10,21 +11,20 @@ class NetworkFormat:
     # -------------------------------------------------------------
     # Load file format from network_file_format
     # -------------------------------------------------------------
-    def __init__(self, name = None):
-        
-        if name != None and name != '':
+    def __init__(self, name=None):
+
+        if name != None and name != "":
             self.createFromFile(name)
         else:
-            self.name = 'UNDEFINED'
-            
-            
+            self.name = "UNDEFINED"
+
     def createFromFile(self, name):
-        
+
         FIELDS = []
         with open(NetworkFormat.NETWORK_FILE_FORMAT) as ffmt:
             line = ffmt.readline().strip()
-            while(line):
-                if (line[0] == "#"):
+            while line:
+                if line[0] == "#":
                     line = ffmt.readline().strip()
                     continue
                 tab = line.split(",")
@@ -33,11 +33,11 @@ class NetworkFormat:
                     break
                 line = ffmt.readline().strip()
         ffmt.close()
-        
+
         if len(FIELDS) < 1:
             print("Error: import format not recognize")
             exit()
-        
+
         self.name = name
         self.pos_edge_id = int(FIELDS[1].strip())
         self.pos_source = int(FIELDS[2].strip())
@@ -48,40 +48,40 @@ class NetworkFormat:
 
         self.h = int(FIELDS[8].strip())
         self.doublequote = True
-        self.encode = 'utf-8'
-        
+        self.encode = "utf-8"
+
         self.srid = FIELDS[11].strip()
-        
+
         self.separator = FIELDS[7].strip()
         self.separator = self.separator.replace("b", " ")
         self.separator = self.separator.replace("c", ",")
         self.separator = self.separator.replace("s", ";")
-    
-    
-    '''
-    '''
+
+    """
+    """
+
     def createFromDict(self, param):
-        
-        if param['name'] != None and param['name'] != '':
-            self.name = param['name']
-        
-        if param['pos_edge_id'] != None and param['pos_edge_id'] != '':
-            self.pos_edge_id = param['pos_edge_id']
-        
-        if param['pos_source'] != None and param['pos_source'] != '':
-            self.pos_source = param['pos_source']
-            
-        if param['pos_target'] != None and param['pos_target'] != '':
-            self.pos_target = param['pos_target']
-            
-        if param['pos_wkt'] != None and param['pos_wkt'] != '':
-            self.pos_wkt = param['pos_wkt']
-            
-        if param['pos_poids'] != None and param['pos_poids'] != '':
-            self.pos_poids = param['pos_poids']
-            
-        if param['pos_sens'] != None and param['pos_sens'] != '':
-            self.pos_sens = param['pos_sens']
-            
-        if param['srid'] != None and param['srid'] != '':
-            self.srid = param['srid']
+
+        if param["name"] != None and param["name"] != "":
+            self.name = param["name"]
+
+        if param["pos_edge_id"] != None and param["pos_edge_id"] != "":
+            self.pos_edge_id = param["pos_edge_id"]
+
+        if param["pos_source"] != None and param["pos_source"] != "":
+            self.pos_source = param["pos_source"]
+
+        if param["pos_target"] != None and param["pos_target"] != "":
+            self.pos_target = param["pos_target"]
+
+        if param["pos_wkt"] != None and param["pos_wkt"] != "":
+            self.pos_wkt = param["pos_wkt"]
+
+        if param["pos_poids"] != None and param["pos_poids"] != "":
+            self.pos_poids = param["pos_poids"]
+
+        if param["pos_sens"] != None and param["pos_sens"] != "":
+            self.pos_sens = param["pos_sens"]
+
+        if param["srid"] != None and param["srid"] != "":
+            self.srid = param["srid"]
--- tracklib/io/NetworkWriter.py
+++ tracklib/io/NetworkWriter.py
@@ -1,33 +1,31 @@
 # -*- coding: utf-8 -*-
 
 
-
 class NetworkWriter:
-
     @staticmethod
     def writeToCsv(network, path="", separator=",", h=1):
-        
+
         if h == 1:
-            output  = "link_id" + separator
+            output = "link_id" + separator
             output += "source" + separator
             output += "target" + separator
             output += "direction" + separator
             output += "wkt\n"
         else:
-            output  = ""
-            
+            output = ""
+
         for idedge in network.EDGES:
             edge = network.EDGES[idedge]
             output += edge.id + separator
             output += edge.source.id + separator
             output += edge.target.id + separator
             output += str(edge.orientation) + separator
-            output += "\"" + edge.geom.toWKT() +"\""
+            output += '"' + edge.geom.toWKT() + '"'
             output += "\n"
-        
+
         if path:
             f = open(path, "w")
             f.write(output)
             f.close()
-            
+
         return output
