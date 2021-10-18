06223e52b926fefd73866975cd9d359abadd0e61
Mattia Bunel <mattia.bunel@ign.fr>
2021-10-18 17:52:21 +0200

    Correctif read the docs

--- setup.py
+++ setup.py
@@ -4,48 +4,35 @@ from setuptools import setup, find_packages
 
 current_path = os.path.abspath(os.path.dirname(__file__))
 
-requirements = (
-    'numpy',
-    'matplotlib',
-    'scikit-image',
-    'progressbar2'
-)
+requirements = ("numpy", "matplotlib", "scikit-image", "progressbar2")
 
-dev_requirements = (
-    'pytest',
-    'pytest-runner'
-)
+dev_requirements = ("pytest", "pytest-runner")
 
 doc_requirements = (
-    'sphinx',
-    'sphinx_rtd_theme',
-	'recommonmark'
+    "sphinx",
+    "sphinx_rtd_theme",
+    "recommonmark",
+    "sphinx-autodoc-typehints",
 )
 
 
 setup(
-    name='tracklib',
-    version='1.0.0',
+    name="tracklib",
+    version="1.0.0",
     description="Python module providing the main objects to manipulate GPS traces with a variety of tools, operators and functions to perform spatial analysis using these objects",
-	long_description='See https://github.com/umrlastig/tracklib',
-    url='https://github.com/umrlastig/tracklib',
-    author='Yann Méneroux, Marie-Dominique Van Damme',
-    author_email='todo@ign.fr',
-    license='cecill-c',
-    python_requires='>=3.5',
+    long_description="See https://github.com/umrlastig/tracklib",
+    url="https://github.com/umrlastig/tracklib",
+    author="Yann Méneroux, Marie-Dominique Van Damme",
+    author_email="todo@ign.fr",
+    license="cecill-c",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 1 - Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.5",
     ],
-    packages=find_packages(include=['test', 'resources']),
+    packages=find_packages(include=["test", "resources"]),
     install_requires=requirements,
     test_suite="tests",
-    extras_require={
-        'dev': dev_requirements,
-        'doc': doc_requirements
-    },
+    extras_require={"dev": dev_requirements, "doc": doc_requirements},
 )
-
-
-
