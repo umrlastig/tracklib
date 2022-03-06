737ecd923ed9feb799d1a80b91dd3d0e764460c5
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-03-06 20:16:39 +0100

    exemples de simplification

---
+++ example/Simplify.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tracklib.io.FileReader import FileReader
+import tracklib.algo.Simplification as Simplification
+
+
+chemin = './data/lacet/ecrins.csv'
+tracks = FileReader.readFromWKTFile(chemin, 0, 1, 2, ",", 1, 
+                                    "ENUCoords", None, True)
+trace = tracks["903959","%"][0]
+print (trace.size())
+trace = trace.extract(70,120)
+
+#trace.toENUCoords()
+trace.summary()
+
+# ---------------------------------------------------
+tolerance = 3
+trace1 = Simplification.simplify(
+             trace, tolerance, 
+			 Simplification.MODE_SIMPLIFY_SQUARING
+)
+trace.plot(append = False, sym='g-')
+trace1.plot(append = True, sym='b-')
+
+
+# ---------------------------------------------------
+tolerance = 20
+trace2 = Simplification.simplify(
+             trace, tolerance, 
+			 Simplification.MODE_SIMPLIFY_DOUGLAS_PEUCKER
+)
+trace.plot(append = False, sym='g-')
+trace2.plot(append = True, sym='b-')
