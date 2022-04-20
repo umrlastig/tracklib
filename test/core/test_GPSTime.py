56c9305027f4b10c433c68115ffb04e62764010b
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-04-20 21:36:08 +0200

    compare time until second

--- test/core/test_GPSTime.py
+++ test/core/test_GPSTime.py
@@ -20,11 +20,11 @@ class TestGPSTime(unittest.TestCase):
         GPSTime.setReadFormat("4Y-2M-2DT2h:2m:2s1Z")
         madate = '2018-01-31T11:17:46Z'
         t = GPSTime.readTimestamp(madate)
-        self.assertEqual("31/01/2018 11:17:46", str(t))
+        self.assertEqual("31/01/2018 11:17:46", str(t)[0:19])
         
         d = GPSTime.readUnixTime(1550941038.0)
         self.assertIsInstance(d, GPSTime)
-        self.assertEqual('23/02/2019 16:57:18', str(d))
+        self.assertEqual('23/02/2019 16:57:18', str(d)[0:19])
         self.assertEqual(23, d.day)
         self.assertEqual(2, d.month)
         self.assertEqual(2019, d.year)
