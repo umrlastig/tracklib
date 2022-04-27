e98607730382d25920ec1f3a7712dea82f17604f
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-04-27 14:45:49 +0200

    nettoyage import

--- example/Circle.py
+++ example/Circle.py
@@ -1,50 +1,11 @@
-import os
-import sys
 import math
-import random
-import numpy as np
 import matplotlib.pyplot as plt
 
-from tracklib.core.Operator import Operator
-from tracklib.io.FileReader import FileReader
-from tracklib.io.FileWriter import FileWriter
-from tracklib.core.GPSTime import GPSTime
-from tracklib.core.Track import Track
-from tracklib.core.Coords import ECEFCoords
-from tracklib.core.Coords import GeoCoords
-from tracklib.core.Coords import ENUCoords
-
-from tracklib.algo.Selection import TrackConstraint
 import tracklib.algo.Stochastics as Stochastics
 import tracklib.algo.Synthetics as Synthetics
 import tracklib.algo.Geometrics as Geometrics
-import tracklib.algo.Comparison as Comparison
-import tracklib.algo.Cinematics as Cinematics
-import tracklib.algo.Dynamics as Dynamics
 import tracklib.algo.Interpolation as Interpolation
-import tracklib.algo.Simplification as Simplification
-import tracklib.algo.Segmentation as Segmentation
-import tracklib.algo.Mapping as Mapping
-import tracklib.algo.Filtering as Filtering
-from tracklib.algo.Dynamics import Kalman
 
-from tracklib.io.KmlWriter import KmlWriter
-from tracklib.core.Coords import ENUCoords
-
-from tracklib.io.GpxWriter import GpxWriter
-from tracklib.io.GpxReader import GpxReader
-from tracklib.core.TrackCollection import TrackCollection
-from tracklib.core.Kernel import DiracKernel
-from tracklib.core.Kernel import GaussianKernel
-from tracklib.core.Kernel import ExponentialKernel
-from tracklib.core.Kernel import ExperimentalKernel
-
-from tracklib.core.Obs import Obs
-import tracklib.core.Plot as Plot
-from tracklib.core.Network import Node
-from tracklib.core.Network import Edge
-from tracklib.core.Network import Network
-from tracklib.io.NetworkReader import NetworkReader
 
 # -----------------------------------------------------------------------
 # Example 0: simple circle regression
@@ -66,4 +27,10 @@ def example0():
 
     plt.plot(track.getX(), track.getY(), 'b+')
     plt.show()
+
+
+if __name__ == '__main__':
+    example0()
+
+
 
