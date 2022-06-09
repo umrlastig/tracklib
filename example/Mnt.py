44301fb45bff5a952d25d2c5c04effcccb40779e
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-06-10 00:03:25 +0200

    load mnt

--- example/Mnt.py
+++ example/Mnt.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 
-
-
+from tracklib.core.GPSTime import GPSTime
 from tracklib.io.AsciiReader import AsciiReader
+from tracklib.io.FileReader import FileReader
+# import tracklib.algo.Mapping as mapping
 
 chemin = 'tracklib/data/asc/test.asc'
 grid = AsciiReader.readFromFile(chemin)
-print (grid)
+# print (grid)
+
+GPSTime.setReadFormat("4Y/2M/2D 2h:2m:2s")
+trace = FileReader.readFromFile('tracklib/data/asc/8961191_v2.csv', 
+                                id_E=3, id_N=4, id_U=5, id_T=2, 
+                                separator=",", h=1)
+
+print (trace.size())
+trace.plot()
+
+#mapping.mapOnRaster(track, grid)
 
 
-    
     
