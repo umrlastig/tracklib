d7285d9f6328b44a2a4e908082ef9945373a70e8
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-06-09 23:02:19 +0200

    AsciiReader

--- tracklib/io/AsciiReader.py
+++ tracklib/io/AsciiReader.py
@@ -2,7 +2,7 @@
 
 """
 """
-
+import numpy as np
 
 from tracklib.core import (Bbox, Grid)
 from tracklib.core.Coords import (ENUCoords)
@@ -10,16 +10,40 @@ from tracklib.core.Coords import (ENUCoords)
 
 class AsciiReader:
     
+    CLES = ['ncols', 'nrows', 'xllcorner', 'yllcorner', 'cellsize', 'NODATA_value']
+    
     @staticmethod
     def readFromFile(path, srid="ENUCoords"):
         
         with open (path, 'r') as fichier:
-            ncols = int(fichier.readline()[5:].rstrip())
-            nrows = int(fichier.readline()[5:].rstrip())
-            xllcorner = float(fichier.readline()[9:].rstrip())
-            yllcorner = float(fichier.readline()[9:].rstrip())
-            cellsize = float(fichier.readline()[8:].rstrip())
-
+            lines = fichier.readlines()
+            fichier.close()
+            
+            cptrowheader = 0
+            for line in lines:
+                cle = line.split(" ")[0].strip()
+                if cle in AsciiReader.CLES:
+                    cptrowheader += 1
+                    
+                    i = 1
+                    val = line.split(" ")[1].strip()
+                    while val == '':
+                        i += 1
+                        val = line.split(" ")[i].strip()
+                    
+                    if cle == 'ncols':
+                        ncols = int(val)
+                    if cle == 'nrows':
+                        nrows = int(val)
+                    if cle == 'xllcorner':
+                        xllcorner = float(val)
+                    if cle == 'yllcorner':
+                        yllcorner = float(val)
+                    if cle == 'cellsize':
+                        cellsize = int(float(val))
+                else:
+                    break
+                    
             ll = ENUCoords(xllcorner, yllcorner, 0)
             ur = ENUCoords(xllcorner + cellsize * ncols, yllcorner + cellsize * nrows, 0)
             bbox = Bbox.Bbox(ll, ur)
@@ -28,5 +52,10 @@ class AsciiReader:
             marge = 0
             
             grid = Grid.Grid(bbox, resolution=resolution, margin=marge)
+            
+            matrice = np.loadtxt(path, skiprows=cptrowheader, dtype=np.int16)
+            grid.grid = matrice.T
         
             return grid
+        
+    
