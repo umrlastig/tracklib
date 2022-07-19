3f9c01d0a8cedac9f6e7cfa3f00e76c125c4075e
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-07-19 18:47:59 +0200

    findStopsGlobal

--- test/algo/test_segmentation.py
+++ test/algo/test_segmentation.py
@@ -2,12 +2,12 @@
 
 import os.path
 import unittest
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 from tracklib.core.GPSTime import GPSTime
 from tracklib.io.GpxReader import GpxReader
 #from tracklib.io.FileReader import FileReader
-from tracklib.core.Plot import Plot
+#from tracklib.core.Plot import Plot
 
 from tracklib.algo.Segmentation import findStopsGlobal#, findStopsLocal
 
@@ -17,34 +17,37 @@ class TestAlgoSegmentationMethods(unittest.TestCase):
         self.resource_path = os.path.join(os.path.split(__file__)[0], "../..")
 
 
-    def testFindStopsLocal(self):
-        #GPSTime.setReadFormat("4Y-2M-2D 2h:2m:2s")
+    def testStopsAFaire(self):
+        GPSTime.setReadFormat("4Y-2M-2D 2h:2m:2s")
         #chemin = os.path.join(self.resource_path, './data/trace1.dat')
         #trace = FileReader.readFromFile(chemin, 2, 3, -1, 4, separator=",")
         
+
+    def testFindStopsLocal(self):
         resource_path = os.path.join(os.path.split(__file__)[0], "../..")
         gpxpath = os.path.join(resource_path, 'data/vincennes.gpx')
         GPSTime.setReadFormat("4Y-2M-2DT2h:2m:2sZ")
         tracks = GpxReader.readFromGpx(gpxpath, srid='ENU')
         trace = tracks.getTrack(0)
-        trace = trace.extract(1150, 2500)
+        #trace = trace.extract(1150, 2500)
         print (trace.size())
 
         
         #trace.summary()
 
-        plot = Plot(trace)
-        plot.plotProfil('SPATIAL_SPEED_PROFIL')
+        #plot = Plot(trace)
+        #plot.plotProfil('SPATIAL_SPEED_PROFIL')
         
-        trace.plot()
-        stops = findStopsGlobal(trace)
+        #trace.plot()
+        
+        stops = findStopsGlobal(trace, downsampling=5)
         print (type(stops), len(stops))
         
         
         #COLS = utils.getColorMap((220, 220, 220), (255, 0, 0))
         #trace.plot(type='POINT', af_name='virage', append = False, cmap = COLS)
     
-        plt.plot(stops.getX(), stops.getY(), 'ro')
+        #plt.plot(stops.getX(), stops.getY(), 'ro')
     
     
         #self.assertLessEqual(3, 5)
