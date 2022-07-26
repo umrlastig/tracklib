047ea8513880fb8b034a8335fad853a52a7caaaf
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-07-26 19:19:44 +0200

    export track to qgis

--- tracklib/io/QgisWriter.py
+++ tracklib/io/QgisWriter.py
@@ -7,9 +7,10 @@ from tracklib.core.TrackCollection import TrackCollection
 
 #import os # This is is needed in the pyqgis console also
 from qgis.PyQt.QtCore import QVariant
+from qgis.PyQt.QtGui import QColor
 from qgis.core import QgsProject, QgsVectorLayer, QgsField
 from qgis.core import QgsPointXY, QgsFeature, QgsGeometry
-from qgis.core import QgsMarkerSymbol
+from qgis.core import QgsMarkerSymbol, QgsLineSymbol
 
 class QgisWriter:
     """
@@ -29,34 +30,58 @@ class QgisWriter:
             collection.addTrack(tracks)
             tracks = collection
         
-        layerPoint = QgsVectorLayer("Point?crs=epsg:2154", "Tracks", "memory")
-        pr = layerPoint.dataProvider()
+        if type == 'POINT':
+            layerTracks = QgsVectorLayer("Point?crs=epsg:2154", "Tracks", "memory")
+        if type == 'LINE':
+            layerTracks = QgsVectorLayer("LineString?crs=epsg:2154", "Tracks", "memory")
+
+            
+        pr = layerTracks.dataProvider()
         pr.addAttributes([QgsField("idtrace", QVariant.Int)])
         pr.addAttributes([QgsField("idpoint", QVariant.Int)])
-        layerPoint.updateFields()
+        layerTracks.updateFields()
 
         for i in range(tracks.size()):
             track = tracks.getTrack(i)
+            
+            ptOld = None
             for j in range(track.size()):
                 obs = track.getObs(j)
                 X = float(obs.position.getX())
                 Y = float(obs.position.getY())
                 pt = QgsPointXY(X, Y)
                 gPoint = QgsGeometry.fromPointXY(pt)
-                fet = QgsFeature()
                 
-                fet.setGeometry(gPoint)
-                fet.setAttributes([i+1, j+1]) 
-                pr.addFeatures([fet])
+                if type == 'POINT':                
+                    fet = QgsFeature()
+                    fet.setAttributes([i+1, j+1]) 
+                    fet.setGeometry(gPoint)
+                    pr.addFeatures([fet])
+                    
+                if type == 'LINE' and ptOld != None:
+                    fet = QgsFeature()
+                    fet.setAttributes([i+1, j+1]) 
+                    fet.setGeometry(QgsGeometry.fromPolylineXY([ptOld, pt]))
+                    pr.addFeatures([fet])
+                
+                ptOld = pt
 
-        symbol = QgsMarkerSymbol.createSimple({
-            'name': 'circle', 
-            'color': 'orange', 
-            'size': '0.8', 
-            'outline_color': 'orange'})
-        layerPoint.renderer().setSymbol(symbol)
+        if type == 'POINT':
+            symbol = QgsMarkerSymbol.createSimple({
+                'name': 'circle', 
+                'color': 'orange', 
+                'size': '0.8', 
+                'outline_color': 'orange'})
+            layerTracks.renderer().setSymbol(symbol)
+        if type == 'LINE':
+            symbolL = QgsLineSymbol.createSimple({
+                'penstyle':'solid', 
+                'width':'0.6',
+                'line_style':'dash'})
+            symbolL.setColor(QColor.fromRgb(255, 127, 0))
+            layerTracks.renderer().setSymbol(symbolL)
 
-        layerPoint.updateExtents()
-        QgsProject.instance().addMapLayer(layerPoint)
+        layerTracks.updateExtents()
+        QgsProject.instance().addMapLayer(layerTracks)
 
         
