a5351304924274054e51f71499c4b6504acf7cc8
mdvandamme <marie-dominique.vandamme@ign.fr>
2022-12-16 20:40:38 +0100

    test simple functions in Track

--- test/core/test_track.py
+++ test/core/test_track.py
@@ -16,13 +16,34 @@ class TestTrack(TestCase):
         p1 = Obs.Obs(c1, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:00"))
         self.trace1.addObs(p1)
         
+        # ---------------------------------------------------------------------
+        self.trace2 = Track.Track([], 1)
+        c1 = Coords.ENUCoords(1.0, 5.0, 0)
+        p1 = Obs.Obs(c1, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:00"))
+        self.trace2.addObs(p1)
+        
+        c2 = Coords.ENUCoords(2.0, 5.0, 0)
+        p2 = Obs.Obs(c2, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:05"))
+        self.trace2.addObs(p2)
+        
+        c3 = Coords.ENUCoords(3.0, 5.0, 0)
+        p3 = Obs.Obs(c3, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:10"))
+        self.trace2.addObs(p3)
+        
+        c4 = Coords.ENUCoords(5.0, 5.0, 0)
+        p4 = Obs.Obs(c4, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:20"))
+        self.trace2.addObs(p4)
+        
+        
         
     def test_str(self):
         obs = self.trace1.getFirstObs()
-        self.assertEqual('01/01/2018 10:00:00', str(obs.timestamp).strip()[0:19])
-        self.assertEqual(1.000, obs.position.getX())
-        self.assertEqual(5.000, obs.position.getY())
-        self.assertEqual(0.000, obs.position.getZ())
+        txt = str(self.trace1)
+        self.assertEqual(txt[0:19], str(obs.timestamp).strip()[0:19])
+        pos = txt[:len(txt)-2].split('[')[1].split(',')
+        self.assertEqual(float(pos[0].split('=')[1].strip()), obs.position.getX())
+        self.assertEqual(float(pos[1].split('=')[1].strip()), obs.position.getY())
+        self.assertEqual(float(pos[2].split('=')[1].strip()), obs.position.getZ())
         
         
     def test_timezone(self):
@@ -36,10 +57,19 @@ class TestTrack(TestCase):
         #self.assertEqual(self.trace1.getFirstObs().timestamp, GPSTime.GPSTime.readTimestamp("2018-01-01 10:00:00"))
         self.assertEqual("01/01/2018 11:00:00", str(self.trace1.getFirstObs().timestamp).strip()[0:19])
         
+        
+    def test_interval(self):
+        f = self.trace2.interval()
+        self.assertEqual(f, 5.00)
+
+        f = self.trace2.interval(mode='spatial')
+        self.assertEqual(f, 1.0)
+        
 
 if __name__ == '__main__':
     suite = TestSuite()
     suite.addTest(TestTrack("test_str"))
     suite.addTest(TestTrack("test_timezone"))
+    suite.addTest(TestTrack("test_interval"))
     runner = TextTestRunner()
     runner.run(suite)
