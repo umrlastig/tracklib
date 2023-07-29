0782e04d942ff6b64a963bdcbc37bf93d508fe82
mdvandamme <marie-dominique.vandamme@ign.fr>
2023-07-29 19:42:19 +0200

    make all modules importable

--- tracklib/plot/__init__.py
+++ tracklib/plot/__init__.py
@@ -3,7 +3,9 @@
 
 from .IPlotVisitor import IPlotVisitor
 from .Plot import *
-from .Qgis import *
 from .MatplotlibVisitor import MatplotlibVisitor
-from .QgisVisitor import QgisVisitor
 
+'''
+from .Qgis import *
+from .QgisVisitor import QgisVisitor
+'''
