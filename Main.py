8caf8fcf7b4268aa0c4515a93ffbfd5b8a6b983e
mdvandamme <marie-dominique.vandamme@ign.fr>
2023-10-13 15:34:10 +0200

    ignore files

--- Main.py
+++ Main.py
@@ -14,7 +14,8 @@ tracks.noise(3, tkl.kernel.ExponentialKernel(100))
 for t in tracks:
      plt.plot(t["x"], t["y"], 'k-', linewidth=.25)
 
-central1 = tkl.algo.comparison.fusion(tracks, lambda A, B : A + B**2);    
+#central1 = tkl.algo.comparison.fusion(tracks, lambda A, B : A + B**2);    
+central1 = tkl.fusion(tracks, lambda A, B : A + B**2);    
 
 central1.plot('r-')
 
