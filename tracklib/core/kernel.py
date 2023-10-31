c477890211f0279610abfb924199693a9069cead
mdvandamme <marie-dominique.vandamme@ign.fr>
2023-10-31 16:26:55 +0100

    licence header

--- tracklib/core/kernel.py
+++ tracklib/core/kernel.py
@@ -42,27 +42,6 @@ knowledge of the CeCILL-C license and that you accept its terms.
 
 Kernels for filtering, smoothing and stochastics simulations.
 
-An example of Kernel simplification with an 
-`Gaussian Filter <https://tracklib.readthedocs.io/en/latest/api/core/core-kernel.html#tracklib.core.Kernel.GaussianKernel>`__:
- 
-    
-.. code-block:: python
-
-  kernel = GaussianKernel(3)
-  trace.operate(Operator.FILTER, "x", kernel, "x_filtered")
-  trace.operate(Operator.FILTER, "y", kernel, "y_filtered")
-  trace.plot(append = False, sym='g-', label='original track')
-  plt.plot(trace.getAnalyticalFeature("x_filtered"), trace.getAnalyticalFeature("y_filtered"), 
-		 'b-', label='simplify:gaussian filter')
-  plt.legend()
-
-
-.. figure:: ../../../../_images/simplify_gaussian_filter.png
-   :width: 450px
-   :align: center
-
-   Figure 1: Simplification with a gaussian kernel filter
-
 """
 
 # For type annotation
