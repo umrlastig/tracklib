6e7f203be048b023a10730877af9b4fc221ff7bc
mdvandamme <marie-dominique.vandamme@ign.fr>
2023-10-31 15:49:31 +0100

    licence header

--- tracklib/algo/comparison.py
+++ tracklib/algo/comparison.py
@@ -1,3 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright Institut National de l'Information Géographique et Forestière (2020)
+Contributors: 
+    Yann Méneroux
+Creation date: 1th november 2020
+
+tracklib library provides a variety of tools, operators and 
+functions to manipulate GPS trajectories. It is a open source contribution 
+of the LASTIG laboratory at the Institut National de l'Information 
+Géographique et Forestière (the French National Mapping Agency).
+See: https://tracklib.readthedocs.io
+ 
+This software is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software. You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL-C
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-C license and that you accept its terms.
+"""
+
 """to manage comparisons of GPS tracks"""
 
 
@@ -448,44 +489,6 @@ def discreteFrechetCouplingMeasure(track1, track2, i, j, ca):
     return ca[i][j]
 
 
-# Union[TrackCollection, Iterable[Track]]
-# Literal["Hausdorff"]
-# bool
-# -> Track
-def medoid (tracks, mode="Hausdorff", verbose=True): 
-    
-    tracks = tracks.copy()
-
-    if isinstance(tracks, list):
-        tracks = tracklib.TrackCollection(tracks)
-    base = tracks.toENUCoordsIfNeeded()
-    medoid = tracks[0].copy() 
-    
-    for i in range(1, len(tracks)):
-        
-        for j in range(len(medoid)):
-            d = 0
-            
-        #diff = differenceProfile(tracks[0], tracks[i], mode=mode, verbose=verbose)
-
-        #for j in range(len(medoid)):
-            #dx = tracks[i][diff["pair", j]].position.getX()
-            #dy = tracks[i][diff["pair", j]].position.getY()
-            #dz = tracks[i][diff["pair", j]].position.getZ()
-            #medoid[j].position.translate(dx, dy, dz)
-
-    #for j in range(len(medoid)):
-    #    medoid[j].position.scale(1.0 / len(tracks))
-        
-    
-    
-    if not base is None:
-        medoid.toGeoCoords(base)
-    
-    return medoid
-    
-    
- 
 def __meanTrack(cluster):
     N = len(cluster)
     x = cluster[0].E
@@ -496,7 +499,8 @@ def __meanTrack(cluster):
         y += cluster[i].N
         z += cluster[i].U
     return tracklib.core.obs_coords.ENUCoords(x/N, y/N, z/N)
-    
+
+
 def fusion(tracks, weight=lambda A, B : A + B**2, ref=0):
 
     ITER_MAX = 100
