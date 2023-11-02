829bd3e14dbdeae89388428227d6ced8f479da5e
mdvandamme <marie-dominique.vandamme@ign.fr>
2023-11-02 10:08:53 +0100

    new package realease

--- setup.py
+++ setup.py
@@ -30,14 +30,14 @@ doc_requirements = (
 
 setup (
     name="tracklib",
-    version="0.6.7",
-    description="Tracklib library provide a variety of tools, operators and functions to manipulate GPS trajectories",
+    version="0.6.8",
+    description="tracklib library provides a variety of tools, operators and functions to manipulate GPS trajectories",
     long_description="See https://github.com/umrlastig/tracklib",
     url="https://github.com/umrlastig/tracklib",
     download_url= 'https://github.com/umrlastig/tracklib/archive/refs/tags/v0.6.7.tar.gz',
     author="Yann Méneroux, Marie-Dominique Van Damme",
     author_email="todo@ign.fr",
-    keywords=['GPS track', 'python', 'map-matching', 'interpolate GPS tracks', 'segmenting GPS tracks','summarizing GPS tracks', 'stop positions', 'Kalman filter'],
+    keywords=['GPS track', 'Python library', 'Map-matching', 'Interpolate GPS tracks', 'Segmenting GPS tracks', 'Summarizing GPS tracks', 'Stop positions', 'Kalman filter'],
     license="Cecill-C",
     python_requires=">=3.8",
     classifiers=[
