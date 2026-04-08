f9171f732d661b287b3e6e00ca4ddb66c5dd8d38
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2026-04-08 11:09:53 +0200

    publish v0.8.0 (download_url)

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup (
     description="tracklib library provides a variety of tools, operators and functions to manipulate GPS trajectories",
     long_description="See https://github.com/umrlastig/tracklib",
     url="https://github.com/umrlastig/tracklib",
-    download_url= 'https://github.com/umrlastig/tracklib/archive/refs/tags/v0.7.3.tar.gz',
+    download_url= 'https://github.com/umrlastig/tracklib/archive/refs/tags/v0.8.0.tar.gz',
     author="Yann Méneroux, Marie-Dominique Van Damme",
     author_email="todo@ign.fr",
     keywords=['GPS track', 'Python library', 'Map-matching', 'Trajectory fusion', 'Synthetic trajectory', 'Segmenting GPS tracks', 'Summarizing GPS tracks', 'Stop positions', 'Kalman filter'],
