14054516efc8b32cec6bb805536b8c22512e2be7
Marie-Dominique Van Damme <marie-dominique.vandamme@ign.fr>
2026-06-19 10:56:44 +0200

    v0.8.3.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ dev_requirements = (
 
 setup (
     name="tracklib",
-    version="0.8.3.4",
+    version="0.8.3.5",
     description="tracklib library provides a variety of tools, operators and functions to manipulate GPS trajectories",
     long_description="See https://github.com/umrlastig/tracklib",
     url="https://github.com/umrlastig/tracklib",
